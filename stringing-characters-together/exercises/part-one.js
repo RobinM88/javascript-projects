634b67ebb492e572327a424da26bb617ef15bd3f
Robin Mennel <robinmennel88@gmail.com>
2024-01-12 12:24:00 -0600

    string characters

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,26 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+let lengthOfNumber = String(num).length;
+console.log(lengthOfNumber)
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+let myDecimal = 123.45;
+// let decimalString = String(myDecimal)
+// let numberOfdigits = decimalString.replace(".", "").length;
+// console.log(numberOfdigits);
+let numberOfdigits = myDecimal.toString().replace(".", "").length;
+console.log(numberOfdigits)
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+// if (String(num).includes('.')){
+//     console.log(String(num).length-1);
+// } else {
+//     console.log(String(num).length);
+// }
+
+if (num.toString().includes(".")) {
+    let lengthOfNumber = num.toString().replace(".", '').length;
+    console.log(lengthOfNumber)
+} else{
+    let lengthOfNumber = num.toString().length;
+    console.log(lengthOfNumber)
+} 
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,24 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+console.log(language.slice(0,1)+language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+console.log(language[0] + language[4])
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+let abreviation = ((language[0])+(language[4]))
+console.log(`The abreviation for JavaScript is ${abreviation}. `)
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
-
+let mixUp = language.replace('a','s')
+console.log(mixUp)
+let weirdWay = mixUp.slice(0,2).toUpperCase()
+console.log(weirdWay)
+console.log(`Weird way to get JavaScript to change to ${weirdWay}`)
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+// let firstWord = notTitleCase.replace(notTitleCase.charAt(0), notTitleCase.charAt(0).toUpperCase());
+// let secondWord = firstWord.replace(firstWord.charAt(6), firstWord.charAt(6).toUpperCase());
+let titleCase = notTitleCase.replace(notTitleCase.charAt(0), notTitleCase.charAt(0).toUpperCase()).replace(notTitleCase.charAt(6), notTitleCase.charAt(6).toUpperCase());
+console.log(titleCase)
