1809c8a77d65546cf9454400315347fac5246387
Robin Mennel <robinmennel88@gmail.com>
2024-01-22 13:37:24 -0600

    excersises done

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,6 +1,6 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-const input = require('readlin-sync');
-let fuelLevel = 0, numAstronauts = 0, altitude = o;
+const input = require('readline-sync');
+let fuelLevel = 0, numAstronauts = 0, altitude = 0;
 
 
 
@@ -15,14 +15,24 @@ while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)){
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+  while (numAstronauts <= 1 || numAstronauts >7 || isNaN(numAstronauts)){
+    numAstronauts = input.question("How many astronauts are aboard?");
+  }
   
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
+while (fuelLevel > 0){
+  fuelLevel -= 100 *numAstronauts;
+  altitude += 50;
+}
 
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
 
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+if (altitude >= 2000){
+  console.log(`The shuttle gained an altitude of ${altitude} km. Orbit achieved! `)
+} else {(`The shuttle gained an altitude of ${altitude} km. Failed to achieve orbit! `)
+
+}
