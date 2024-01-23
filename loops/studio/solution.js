a2a2c7a276eb67810532f43d12dba84faf136e25
Robin Mennel <robinmennel88@gmail.com>
2024-01-22 20:31:55 -0600

    studio complete

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -16,17 +16,25 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
-
+ 
+  for (let i = 0; i < numMeals; i++) {
+    let crewMeals = [];
+    for (let j = 0; j < pantry.length; j++) {
+      crewMeals.push(pantry[j][i])
+    }
+    meals.push(crewMeals);
+  }
+  
 
   return meals;
 }
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
-  
+  numMeals = input.question("How many meals would you like to make? ");
+  while (numMeals > 0|| numMeals <= 6 || isNaN(numMeals))
   /// CODE YOUR SOLUTION TO PART B here ///
-
+ 
   return numMeals;
 }
 
@@ -46,24 +54,35 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  // console.log(meals)
+  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  console.log(meals)
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
-  // let password1 = '';
-  // let password2 = '';
-  // console.log("Time to run the password generator so we can update the menu tomorrow.")
-  // console.log(`The new password is: ${generatePassword(password1, password2)}`);
+  let password1 = 'cake';
+  let password2 = 'smell';
+  for (let i = 0; i < string1.length || 
+    i < string2.length; i++)
+{
+if (i < string1.length)
+code += string1[i]; 
+if (i < string2.length)
+code += string2[i];
+}
+return code;
+
+
+  console.log("Time to run the password generator so we can update the menu tomorrow.")
+  console.log(`The new password is: ${generatePassword(password1, password2)}`);
 }
 
 module.exports = {
