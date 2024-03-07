6c0b04919f702b6746ec297079d70f4bac0978c4
Robin Mennel <robinmennel88@gmail.com>
2024-03-06 21:10:30 -0600

    studio fetch

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,23 @@
 //TODO: Add Your Code Below
+window.addEventListener('load', function() {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('container');   
+        data.forEach(astronaut => {
+            const astronautDiv = document.createElement('div');
+            astronautDiv.classList.add('astronaut');
+            astronautDiv.innerHTML = `
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <li>Active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills.join(', ')}</li>
+                </ul>
+                <img class="avatar" src="${astronaut.picture}" alt="${astronaut.firstName} ${astronaut.lastName}">
+            `;        
+            container.appendChild(astronautDiv);
+        });
+    });
+});
+            
